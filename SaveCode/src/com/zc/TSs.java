54129716841f39adf65cafbeac83ec498ae45168
zhaoccx <zhaoccx@hotmail.com>
2014-10-24 17:37:42 +0800

    Init

--- SaveCode/src/com/zc/TSs.java
+++ SaveCode/src/com/zc/TSs.java
@@ -2,5 +2,11 @@ package com.zc;
 
 
 public class TSs {
-
+	public void Systemget(String se){
+		System.out.println("send");
+	}
+	
+	public static void main(String[] args) {
+		new TSs().Systemget(null);
+	}
 }
