438f9e6e9acfd626f97c325e49a6a598f0b0ea60
趙常春 <zhaoccx@hotmail.com>
2018-04-28 16:34:22 +0800

    Init

---
+++ SaveJavaCode/src/com/zcc/test/InitTests.java
@@ -0,0 +1,12 @@
+package com.zcc.test;
+
+/**
+ * @author zhaocc
+ * @time Apr 28, 20183:49:34 PM
+ * @parent
+ */
+public class InitTests {
+	public static void main(String[] args) {
+		System.out.println("Test is OK");
+	}
+}
