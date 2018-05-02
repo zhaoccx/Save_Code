0f897989025725040b6fe15ce404e7382c40467f
趙常春 <zhaoccx@hotmail.com>
2018-05-02 14:14:30 +0800

    Init
    Signed-off-by: 趙常春 <zhaoccx@hotmail.com>

---
+++ SaveUtilsCode/src/com/zcc/utils/TestUtils.java
@@ -0,0 +1,21 @@
+package com.zcc.utils;
+
+/**
+ * @author zhaocc
+ * @time May 2, 20182:13:44 PM
+ * @parent
+ */
+public class TestUtils {
+
+	/**
+	 * @author zhaocc
+	 * @time May 2, 20182:13:44 PM
+	 * @param args
+	 * @path
+	 * @parent
+	 */
+	public static void main(String[] args) {
+		System.out.println("sxtest");
+	}
+
+}
